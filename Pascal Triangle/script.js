40365ebccc0c80a4da18528d799312527d346e1e
Kim Dumaguing <kimjay.dumaguing@Gmail.com>
2020-07-03 17:01:20 -0700

    Update script.js

--- Pascal Triangle/script.js
+++ Pascal Triangle/script.js
@@ -1,9 +1,8 @@
-const mainContainer = document.getElementById('container');
-
+const mainContainer = document.getElementById("container");
 
 // First two rows:
-const firstRow = [1]
-let prevRow = [1,1];
+const firstRow = [1];
+let prevRow = [1, 1];
 
 const firstRowElement = document.createElement("div");
 firstRowElement.setAttribute("class", "rowContainer");
@@ -11,20 +10,20 @@ firstRowElement.setAttribute("class", "rowContainer");
 const secondRowElement = document.createElement("div");
 secondRowElement.setAttribute("class", "rowContainer");
 
-firstRowElement.innerHTML = "<span class = \"cell\">1</span>";
-secondRowElement.innerHTML = "<span class = \"cell\">1</span><span class = \"cell\">1</span>";
+firstRowElement.innerHTML = '<span class = "odd">1</span>';
+secondRowElement.innerHTML = '<span class = "odd">1</span><span class = "odd">1</span>';
 
 mainContainer.appendChild(firstRowElement);
 mainContainer.appendChild(secondRowElement);
 
 // Pascal Triangle
-const rows = 20;
+const rows = 30;
 
-for (let i = 3; i <= rows; i++){
+for (let i = 3; i <= rows; i++) {
   let row = [];
 
-  for (let j = 0; j < prevRow.length - 1; j++){
-    row.push(prevRow[j]+prevRow[j+1]);
+  for (let j = 0; j < prevRow.length - 1; j++) {
+    row.push(prevRow[j] + prevRow[j + 1]);
   }
   row.unshift(1);
   row.push(1);
@@ -32,26 +31,24 @@ for (let i = 3; i <= rows; i++){
   prevRow = row;
 
   let rowContainer = document.createElement("div");
-  rowContainer.setAttribute("class","rowContainer");
+  rowContainer.setAttribute("class", "rowContainer");
 
   // take all array elements, and put them in a cell and appendChild to a row div
-  for (let k = 0; k < row.length; k++){
+  for (let k = 0; k < row.length; k++) {
     let rowCell = document.createElement("span");
     rowCell.setAttribute("class", "cell");
     rowCell.innerHTML = row[k];
-    if (isOdd(row[k])){
+    if (isOdd(row[k])) {
       rowCell.setAttribute("class", "odd");
     }
     rowContainer.appendChild(rowCell); // will it work?
   }
 
   mainContainer.appendChild(rowContainer);
-
 }
 
-
 function isOdd(num) {
-  if (num%2 == 0){
+  if (num % 2 == 0) {
     return false;
   }
   return true;
